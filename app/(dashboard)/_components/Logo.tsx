bb4e0e89da3c6917333623c3ecd6b19c19d8d933
Hamza-benfaddoul <benfaddoul01@gmail.com>
2024-05-16 19:13:11 +0100

    update logo

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 
 const Logo = () => {
-  return <Image height={130} width={130} alt='logo' src='/logo.jpg' />
+  return <Image height={200} sizes='lg' width={230} alt='logo' src='/logo.png' />
 }
 
 export default Logo
