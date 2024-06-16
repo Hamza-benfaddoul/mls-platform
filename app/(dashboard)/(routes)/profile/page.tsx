b8b8a1cba93e699c9f39691b0ad7832f26631ab4
Hamza-benfaddoul <benfaddoul01@gmail.com>
2024-06-16 22:56:48 +0100

    update

---
+++ app/(dashboard)/(routes)/profile/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div className='flex justify-center'>
+    <UserButton/>
+    </div>
+  )
+}
+
+export default ProfilePage
